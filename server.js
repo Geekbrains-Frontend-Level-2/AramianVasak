8698c9c18809f37c62ca1a4dbe3a85dc701b6a7a
Vasak Aramian <yooopi.av@gmail.com>
2020-05-11 17:42:04 +0500

    Lesson 1 style fix

--- server.js
+++ server.js
@@ -12,11 +12,9 @@ const server = http.createServer((req, res) => {
     }
 
     res.end(body);
-
 })
 
 const port = process.env.PORT || 3000;
-
 server.listen(port);
 
 console.log('Server started on port: '+ port);
